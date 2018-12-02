f5ef2dfd1c0e1c0778c3384068c325ba337fb0e2
Alex <alexander09cruz@hotmail.com>
2018-12-02 13:53:24 -0600

    Se crea la migracion de articulos

---
+++ database/migrations/2018_12_02_194141_create_articulos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('descripcion');
+            $table->string('id_categoria');
+            $table->string('imagen');
+            $table->string('nombre');
+            $table->double('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articulos');
+    }
+}
