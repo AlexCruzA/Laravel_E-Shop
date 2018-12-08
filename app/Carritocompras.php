b421853ad64efa04f737129f35eeedcc574857be
Alex <alexander09cruz@hotmail.com>
2018-12-08 13:19:06 -0600

    Edicion del modelo de carritocompras

--- app/Carritocompras.php
+++ app/Carritocompras.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Carritocompras extends Model
 {
-    //
+    protected $table = 'carritocompras';
+    protected $fillable = ['descripcion','id_categoria', 'imagen', 'nombre', 'precio'];
 }
