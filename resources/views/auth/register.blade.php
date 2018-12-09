1b2baff3901e38fad65008f46cbcab9d43212860
Alex <alexander09cruz@hotmail.com>
2018-12-09 13:13:32 -0600

    Se editan los usuarios para agregarles categoria

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,26 @@
                             </div>
                         </div>
 
+                         <div class="form-group row">
+                            <label for="rol" class="col-md-4 col-form-label text-md-right">{{ __('Rol') }}</label>
+
+                            <div class="col-md-6">
+
+                                <select id="rol" type="text" class="form-control{{ $errors->has('rol') ? ' is-invalid' : '' }}" name="rol" value="{{ old('rol') }}">
+                                  <option value="Administrador">Administrador</option>
+                                  <option value="Comprador">Comprador</option>
+                                </select>
+
+                               
+
+                                @if ($errors->has('rol'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('rol') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
